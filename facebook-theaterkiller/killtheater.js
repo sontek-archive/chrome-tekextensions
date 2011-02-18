3afe2ce3d58c2167396aba154d034d84a3cb66d0
John Anderson <sontek@gmail.com>
2011-02-18 15:20:48 -0600

    modified anchors as well

--- facebook-theaterkiller/killtheater.js
+++ facebook-theaterkiller/killtheater.js
@@ -1,9 +1,31 @@
 function kill_theater() {
-	var url = window.location.href;
-	if (url.indexOf("&theater") !=-1) {
-		console.log("in theater... redirecting");
-		window.location.href = url.split("&theater")[0];
-	}
+    // kill theater anchors
+    var anchors = document.getElementsByTagName("a");
+    console.log("got anchors " + anchors.length);
+
+    for(var index = 0; index < anchors.length; index++)
+    {
+        current_anchor = anchors[index];
+
+        // handle click of anchored images, and redirect
+        if(current_anchor != null && (current_anchor.rel == "theater" 
+            || current_anchor.className.match(/uiMediaThumb|uiPhotoThumb/)))
+        {
+            current_anchor.addEventListener("click", function(e)
+            {
+                window.location.assign(this.href);
+                return false;
+            }, false);
+        }
+    }
+
+    // check the URL just because facebook
+    // does funky things with document.load
+    var url = window.location.href;
+    if (url.indexOf("&theater") !=-1) {
+        console.log("in theater... redirecting");
+        window.location.href = url.split("&theater")[0];
+    }
 }
 
 setInterval ( "kill_theater()", 1000 );
